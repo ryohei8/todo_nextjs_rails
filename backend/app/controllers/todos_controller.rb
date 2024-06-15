585708749afec5b708f6c341d8297e414455f885
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-06-15 22:32:49 +0900

    define show action

--- backend/app/controllers/todos_controller.rb
+++ backend/app/controllers/todos_controller.rb
@@ -1,9 +1,18 @@
 class TodosController < ApplicationController
   
+  # Todo一覧
   def index
     # 日付が新しい順に10件表示する
     @todos = Todo.all.order(created_at: :desc).limit(10)
 
     render json: @todos
   end
+
+  # GET/todos/:id
+  def show
+    @todo = Todo.find(params[:id])
+
+    render json: @todo
+  end
+
 end
