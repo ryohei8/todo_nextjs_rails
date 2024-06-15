1ee6812c7833b758ddd7e449f50de9b3a0f6cb16
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-06-15 15:51:02 +0900

    Prepare data

--- backend/app/controllers/todos_controller.rb
+++ backend/app/controllers/todos_controller.rb
@@ -1,2 +1,9 @@
 class TodosController < ApplicationController
+  
+  def index
+    # 日付が新しい順に10件表示する
+    @todos = Todo.all.order(created_at: :desc).limit(10)
+
+    render json: @todos
+  end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Todo.create(title: '部屋の掃除', content: '本棚の中身')
+Todo.create(title: '玄関の掃除', content: '靴箱の中身')
+Todo.create(title: '風呂の掃除', content: 'バスタブを念入りに')
