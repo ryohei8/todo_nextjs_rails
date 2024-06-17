60f373205d82e7895ef22e23344e57ba231456c4
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-06-17 12:29:20 +0900

    Show creating todo form on index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,10 @@
 import Todos from "@/components/Todos";
+import CreateTodoForm from "@/components/CreateTodoForm";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
+      <CreateTodoForm />
       <Todos />
     </div>
   );
